a0537768c540b9a770b9633d55acdc225f7b0a57
jburto94 <jburto94@yahoo.com>
2018-08-07 13:45:57 -0400

    Added initial server routes

---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+      router = express.Router();
+
+// @route GET /api/profile/test
+// @desc Tests profile route
+// @access Public
+router.get('/test', (req, res) => {
+  res.json({ message: 'Profile works' });
+});
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require('express'),
+      router = express.Router();
+
+// @route GET /api/users/test
+// @desc Tests users route
+// @access Public
+router.get('/test', (req, res) => {
+  res.json({ message: 'Users works' });
+});
+
+module.exports = router;
